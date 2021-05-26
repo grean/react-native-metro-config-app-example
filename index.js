87ba5ae500c406a522a4ff492341987d1f96f765
Réan Guillaume <rean.guillaume@gmail.com>
2021-05-26 20:49:56 +0200

    init

--- index.js
+++ index.js
@@ -5,42 +5,8 @@
  * @format
  */
 
-var list = [/\/__tests__\/.*/];
-
-function escapeRegExp(pattern) {
-  if (Object.prototype.toString.call(pattern) === '[object RegExp]') {
-    // the forward slash may or may not be escaped in regular expression depends
-    // on if it's in brackets. See this post for details
-    // https://github.com/nodejs/help/issues/3039. The or condition in string
-    // replace regexp is to cover both use cases.
-    // We should replace all forward slashes to proper OS specific separators.
-    // The separator needs to be escaped in the regular expression source string,
-    // hence the '\\' prefix.
-    return pattern.source.replace(/\/|\\\//g, '\\' + path.sep);
-  } else if (typeof pattern === 'string') {
-    // Make sure all the special characters used by regular expression are properly
-    // escaped. The string inputs are supposed to match as is.
-    var escaped = pattern.replace(/[\-\[\]\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
-    // convert the '/' into an escaped local file separator. The separator needs
-    // to be escaped in the regular expression source string, hence the '\\' prefix.
-    return escaped.replace(/\//g, '\\' + path.sep);
-  } else {
-    throw new Error('Unexpected exclusion pattern: ' + pattern);
-  }
-}
-
-function exclusionList(additionalExclusions) {
-  return new RegExp(
-    '(' +
-    (additionalExclusions || [])
-      .concat(list)
-      .map(escapeRegExp)
-      .join('|') +
-    ')$',
-  );
-}
-
 const pak = require('../package.json');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
 const path = require('path');
 
